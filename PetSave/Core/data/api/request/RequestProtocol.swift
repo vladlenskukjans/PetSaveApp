371360f31fc94bb545ef63ce49f8fb0c252b66db
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-04-25 12:10:17 +0100

    add new resquest Type enums

--- PetSave/Core/data/api/request/RequestProtocol.swift
+++ PetSave/Core/data/api/request/RequestProtocol.swift
@@ -3,19 +3,14 @@ import Foundation
 
 protocol RequestProtocol {
   var path: String { get }
-  
   var headers: [String: String] { get }
   var params: [String:Any] { get }
-  
   var urlParams: [String: String?] { get }
-  
   var addAuthorizationToken: Bool { get }
-  
   var requestType: RequestType { get }
 }
 
 extension RequestProtocol {
-  
   var host: String {
     APIConstants.host
   }
